db0841dfc6e499e980128b8c54bb62036531eb68
Roy Ash <roykid123@gmail.com>
2018-12-18 18:12:03 +0200

    sdf

---
+++ test/code-analyzer.test.ts
@@ -0,0 +1,34 @@
+/* eslint-disable max-lines-per-function */
+import assert from 'assert';
+import * as codeAnalyzer from '../src/js/code-analyzer';
+
+describe('The javascript parser', () => {
+    // function pushLine(line, type, name = '', condition = '', value = '') {
+    //     return {line: line, type: type, name: name, condition: condition, value: value};
+    // }
+
+    it('testing deepStrictEqual ' +
+        '\'let a=4;\n' +
+        'a;\n' +
+        'function foo (x, y, z) {\n' +
+        '    a;\n' +
+        '}\'', () => {
+        assert.deepEqual(codeAnalyzer.parseCode('', '1, 2, 3'), {type: 'Program', body: [], sourceType: 'script'});
+    });
+
+    // it('testing deepStrictEqual ' +
+    //     '\'let a=4;\n' +
+    //     'a;\n' +
+    //     'function foo (x, y, z) {\n' +
+    //     '    a;\n' +
+    //     '}\'', () => {
+    //     assert.deepStrictEqual(codeAnalyzer.parseCode('let a=4;\n' +
+    //         'a;\n' +
+    //         'function foo (x, y, z) {\n' +
+    //         '    a;\n' +
+    //         '}', '1, 2, 3'), '4;\n' +
+    //         'function foo(x, y, z) {\n' +
+    //         '    4;\n' +
+    //         '}');
+    // });
+});
