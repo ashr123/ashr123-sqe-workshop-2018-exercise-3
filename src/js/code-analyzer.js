2f12d613aa4df8ff73a164cc25ccb3ab9fca7c85
Roy Ash <roykid123@gmail.com>
2019-01-01 15:43:03 +0200

    lkm

--- src/js/code-analyzer.js
+++ src/js/code-analyzer.js
@@ -1,19 +1,15 @@
 import {parseScript} from 'esprima';
-import {initParams, removeUndefinedElements, substituteStatementListItem} from './simbolicSubsExp';
+import {initParams, substituteStatementListItem} from './simbolicSubsExp';
 
-export const parseCode = (codeToParse, params) => {//TODO read all VariableDecleration statements and then handle the rest
+export const parseCode = (codeToParse, params) => {
     const parsedCode = parseScript(codeToParse),
         varTable = new Map(),
         verticesTable = [],
         edgesTable = [];
     initParams(params);
-    for (const i in parsedCode.body) {
-        substituteStatementListItem(varTable, parsedCode.body[i], verticesTable, edgesTable);
-        if (parsedCode.body[i].type === 'VariableDeclaration' ||
-            (parsedCode.body[i].type === 'ExpressionStatement' &&
-                parsedCode.body[i].expression.type === 'AssignmentExpression'))
-            delete parsedCode.body[i];
-    }
-    removeUndefinedElements(parsedCode.body);
+    for (const i in parsedCode.body)
+        { // noinspection JSUnfilteredForInLoop
+            substituteStatementListItem(varTable, parsedCode.body[i], verticesTable, edgesTable);
+        }
     return parsedCode;
 };
